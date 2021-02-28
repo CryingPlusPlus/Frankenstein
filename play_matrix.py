22c09ab372860da902f63ff02a121956a36ff1c2
unknown <swernigge123@gmail.com>
2021-02-28 16:20:35 +0100

    build first NN no training yet

--- play_matrix.py
+++ play_matrix.py
@@ -11,5 +11,41 @@ b = np.matrix([
     [11, 12]
 ])
 
-c = a * b
-print(c)
+# c = a * b
+# print(c)
+
+class NN:
+    def __init__(self):
+        self.w = [
+            np.matrix([
+                [1, 2, 3, 4],
+                [1, 2, 3, 4],
+                [1, 2, 3, 4]
+            ]),
+            np.matrix([
+                [1, 2, 3],
+                [1, 2, 3],
+                [1, 2, 3],
+                [1, 2, 3]
+            ]),
+            np.matrix([
+                [1, 2, 3, 4]
+            ])
+        ]
+
+    def y(self, X, l = 2):
+        if l == 0:
+            return self.w[0] * X
+        else:
+            return self.w[l] * self.y(X, l - 1)
+
+n = NN()
+
+X = np.matrix([
+    [1],
+    [2],
+    [3],
+    [4],
+])
+
+print(n.y(X))
